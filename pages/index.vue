c3baeb5251dc8cb4af657fdcad82f03135b808cd
captproton <rocketeer.captproton@gmail.com>
2019-11-12 11:07:34 -0800

    ADDED: hero and call to action boxes with content

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,55 @@
 <template>
-  <div>
+  <main>
     <!-- start -->
-    <div class="hero">
-      <div class="hero-header"> 
-        <h1>bay area CAR INSURANCE</h1>
+    <section class="hero">
+          <h1>SF BAY AREA INSURANCE</h1>
+    <article>
+      <p>"This is the precept by which I have lived: Prepare for the worst; expect the best; and take what comes." </p>
+      <p>~ Hannah Arendt</p>
+    </article>
+
+      <!-- <div class="hero-header">  -->
+        <!-- <h1>bay area CAR INSURANCE</h1> -->
         <!-- background photo credit: https://www.twenty20.com/photos/f221bd8f-d1ec-4f61-bba7-b2e5d797ff3e/?utm_t20_channel=bl -->
-      </div>
-    </div> 
-    <!-- end -->
-  </div>
+      <!-- </div> -->
+    </section> 
+
+    <div class="cta-box secondary-background-color">
+      <ul>
+        <li>
+          <h3 class="fl-heading secondary-color">
+            <span class="fl-heading-text">GET A FREE ATLANTA INSURANCE QUOTE</span>
+          </h3>
+        </li>
+        <li>
+            <button class="bg-green-500 
+              hover:bg-green-400 
+              text-white font-bold 
+              py-2 px-4 
+              border-b-4 
+              border-green-700 
+              hover:border-green-500 
+              rounded
+              w-10/12">
+            Instant Quote
+          </button>
+        </li>
+        <li >
+          <button class="bg-white
+            hover:bg-gray-400 
+            text-blue-500
+            font-bold 
+            py-2 
+            px-4 
+            rounded
+            w-10/12">
+            Instant Quote
+          </button>
+        </li>
+      </ul>
+    </div>     
+<!-- end -->
+  </main>
 </template>
 
 <script>
@@ -18,7 +59,17 @@
 </script>
 
 <style lang="scss" scoped>
-.hero {
+.container {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+}
+.secondary-color {
+  color: #ffffff;
+}
+.secondary-background-color {
+  background: #1a92cd;
+}
+/* .hero {
 display: grid;
 grid-template-columns: repeat(5, 1fr);
 grid-template-rows: repeat(5, 1fr);
@@ -26,29 +77,64 @@ grid-column-gap: 0px;
 grid-row-gap: 0px;
 align-items: stretch;
 }
+*/
+/* Styles for the hero image */
 .hero {
-  background: center / contain no-repeat url('~assets/images/road-trip-to-nowhere_t20_oow6yx.jpg');
-  background-repeat: no-repeat;
-  box-sizing: border-box;
-  width: 100%;
-  height: 87vh; // Always 100% of the viewport's height
-  padding: 20px;
+  /* Photo by mnm.all on Unsplash */
+  background: url('~assets/images/road-trip-to-nowhere_t20_oow6yx.jpg') center;
+  background-size: cover;
+  padding: 4rem 2rem;
+  /* grid styles */
   display: grid;
-  grid-template-rows: 1fr 1fr 1fr 1fr 1fr; // 5 rows
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr; // 5 columns
-  grid-gap: 5px; // 5x5 works well for centering
-}
-.hero-header { 
-  grid-area: 3 / 2 / 4 / 5; 
-  color: #ffffff;
-  border: 0;
-  margin: 0;
-  padding: 0;
-  font-family: Montserrat, sans-serif;
-  font-weight: 500;
-  font-size: 50px;
-  text-align: center;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  align-items: center;
+  height: 75vh;
 }
 
+.hero > * {
+  color: white;
+}
+
+.hero > h1 {
+  font-size: 4rem;
+  padding-bottom: 1rem;
+}
+
+.hero > article > p {
+  font-size: 1.2rem;
+  font-weight: 200;
+}
+
+.hero > article > a {
+  padding: 1rem;
+  margin-top: .75rem;
+}
+
+/* Styles for Call to Action */
+.cta-box {
+  background: #1a92cd;
+  height: 25vh;
+
+}
+.cta-box > ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  justify-content: center;
+  align-items: center;
+}
+
+.cta-box > ul > li {
+  border-color:  #1a92cd;
+  background: #1a92cd;
+
+}
+
+
+.fl-heading {
+  font-family: Montserrat, sans-serif;
+  font-weight: 600;
+  font-size: 24px;
+  text-align: left;
+}
 
 </style>
