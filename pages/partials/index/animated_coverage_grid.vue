fa22556d3e75259813237a7bbc404f7fa2fca0cb
captproton <rocketeer.captproton@gmail.com>
2019-11-17 08:40:47 -0800

    WIP: AnimatedCoverageGrid

---
+++ pages/partials/index/animated_coverage_grid.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="animated-coverage-grid">
+        
+		<div class="uabb-module-content uabb-ib2-outter uabb-new-ib uabb-ib-effect-style1   ">
+	        <h1>animated coverage grid</h1>
+
+			<img class="uabb-new-ib-img" src="https://atlantainsurance.com/wp-content/uploads/2014/05/shutterstock_495196522-1.jpg" alt="atlanta car insurance property coverage">
+			<div class="uabb-new-ib-desc">
+			    <h4 class="uabb-new-ib-title uabb-simplify">PROPERTY</h4>
+			    <div class="uabb-new-ib-content uabb-text-editor uabb-simplify">
+                    <p><span style="font-weight: 400;">Property damage liability covers your legal obligation to the other driver if you're in an accident and damage their car (or if you damage other property.)&nbsp;</span></p>
+                </div>
+	        </div>
+	    </div>
+		<div class="uabb-module-content uabb-ib2-outter uabb-new-ib uabb-ib-effect-style1   ">
+	        <h1>animated coverage grid</h1>
+
+			<img class="uabb-new-ib-img" src="https://atlantainsurance.com/wp-content/uploads/2014/05/shutterstock_495196522-1.jpg" alt="atlanta car insurance property coverage">
+			<div class="uabb-new-ib-desc">
+			    <h4 class="uabb-new-ib-title uabb-simplify">PROPERTY</h4>
+			    <div class="uabb-new-ib-content uabb-text-editor uabb-simplify">
+                    <p><span style="font-weight: 400;">Property damage liability covers your legal obligation to the other driver if you're in an accident and damage their car (or if you damage other property.)&nbsp;</span></p>
+                </div>
+	        </div>
+	    </div>
+		<div class="uabb-module-content uabb-ib2-outter uabb-new-ib uabb-ib-effect-style1   ">
+	        <h1>animated coverage grid</h1>
+
+			<img class="uabb-new-ib-img" src="https://atlantainsurance.com/wp-content/uploads/2014/05/shutterstock_495196522-1.jpg" alt="atlanta car insurance property coverage">
+			<div class="uabb-new-ib-desc">
+			    <h4 class="uabb-new-ib-title uabb-simplify">PROPERTY</h4>
+			    <div class="uabb-new-ib-content uabb-text-editor uabb-simplify">
+                    <p><span style="font-weight: 400;">Property damage liability covers your legal obligation to the other driver if you're in an accident and damage their car (or if you damage other property.)&nbsp;</span></p>
+                </div>
+	        </div>
+	    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
